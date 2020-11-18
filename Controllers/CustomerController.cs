9b842f054bb139949ef580f2d8bb28f11147ee36
Luis Chang <luis.chang@outlook.com>
2020-11-18 13:07:48 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NETCOREM3_DatabaseFirst_EF.DataAccess;
+using NETCOREM3_DatabaseFirst_EF.Models;
+
+namespace NETCOREM3_DatabaseFirst_EF.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Listado()
+        {
+            ViewBag.ListadoClientes = DACustomer.Listado();
+            return PartialView();
+        }
+
+        public IActionResult Grabar(int idCliente, 
+                                    string nombres,
+                                    string apellidos,
+                                    string pais,
+                                    string ciudad,
+                                    string telefono) 
+        {
+            Customer customer = new Customer();
+            customer.FirstName = nombres;
+            customer.LastName = apellidos;
+            customer.City = ciudad;
+            customer.Country = pais;
+            customer.Phone = telefono;
+
+            bool exito = true;
+
+            if (idCliente == -1) //Es un nuevo registro
+                exito = DACustomer.Insertar(customer);
+            else {
+                //Es una actualización
+                customer.Id = idCliente;
+                exito = DACustomer.Actualizar(customer);            
+            }
+
+            return Json(exito);
+        }
+
+        [HttpPost]
+        public IActionResult Eliminar(int idCliente)
+        {
+            bool exito = DACustomer.Eliminar(idCliente);
+            return Json(exito);
+        }
+
+        public IActionResult Obtener(int idCliente)
+        {
+            Customer customer = DACustomer.Obtener(idCliente);
+            return Json(customer);
+        }
+
+
+
+    }
+}
---
+++ DataAccess/DACustomer.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NETCOREM3_DatabaseFirst_EF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NETCOREM3_DatabaseFirst_EF.DataAccess
+{
+    public class DACustomer
+    {
+        public static List<Customer> Listado()
+        {
+            using (var data = new SalesContext())
+            {
+                return data.Customers.OrderBy(x=>x.Country).ToList();
+            }        
+        }
+
+        public static async Task<List<Customer>> ListadoAsync()
+        {
+            using (var data = new SalesContext())
+            {
+                return await data.Customers.ToListAsync();
+            }
+        }
+
+        public static List<Customer> ListadoConSP()
+        {
+            using (var data = new SalesContext())
+            {
+                return data.Customers.FromSqlRaw("SP_SEL_CUSTOMER").ToList();
+            }
+        }
+
+        public static Customer Obtener(int id)
+        {
+
+            using (var data = new SalesContext())
+            {
+                return data.Customers.Where(x => x.Id == id).FirstOrDefault();
+            }
+        }
+
+        public static bool Insertar(Customer customer)
+        {
+            bool exito = true;
+
+            try
+            {
+                using (var data = new SalesContext())
+                {
+                    data.Customers.Add(customer);
+                    data.SaveChanges();
+                }
+
+            }
+            catch (Exception)
+            {
+                exito = false;
+            }
+            return exito;
+        }
+
+
+        public static bool Actualizar(Customer customer)
+        {
+            bool exito = true;
+
+            try
+            {
+                using (var data = new SalesContext())
+                {
+                    Customer customerNow = data.Customers.Where(z => z.Id == customer.Id).FirstOrDefault();
+
+                    customerNow.FirstName = customer.FirstName;
+                    customerNow.LastName = customer.LastName;
+                    customerNow.City = customer.City;
+                    customerNow.Country = customer.Country;
+                    customerNow.Phone = customer.Phone;
+                    data.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+                exito = false;
+            }
+
+            return exito;
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool exito = true;
+            try
+            {
+                using (var data = new SalesContext())
+                {
+                    Customer customer = data.Customers.Where(p => p.Id == id).FirstOrDefault();
+
+                    data.Customers.Remove(customer);
+                    data.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+                exito = false;
+            }
+
+
+            return exito;
+        }
+
+
+
+    }
+}
